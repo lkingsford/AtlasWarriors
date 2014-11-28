d05fe4ed52df5f247bd1e22b5f63c92ddd0bf288
Lachlan Kingsford <lachlan@nerdygentleman.com>
2014-11-28 12:05:57 +1100

    Allow keyboard control of the inventory screen
    Added keyboard shortcuts to inventory, allowed keyboard navigation of
    context menus.

--- src/context.py
+++ src/context.py
@@ -13,24 +13,32 @@ class Context():
         self.Box = pygame.Rect(0,0,0,0)
         self.seperation = 14
         self.width = 0
+        self.curLetter = 'a'
     
     def AddItem(self, item):
-        self.options.append((item, self.font.render(item[0],
-            True, (255, 255, 255, 255)),
+        self.options.append(
+            (item, # 0
+            self.font.render("(" + self.curLetter + ") " + item[0], # 1
+            True, (255, 255, 255, 255)), 
             pygame.Rect(self.x, self.y + self.seperation * len(self.options),
-                self.font.size(item[0])[0], self.seperation)))
+                self.font.size("(" + self.curLetter + ") " + item[0])[0], 
+                self.seperation), # 2
+                self.curLetter)) # 3
         self.width = max(i[2][2] for i in self.options)
         for i in self.options:
             i[2][2] = self.width
         self.Box = pygame.Rect(self.x, self.y, self.width, self.seperation * len(self.options))
-        
+        self.curLetter = chr(ord(self.curLetter) + 1);
+                
     def draw(self, win):
         pygame.draw.rect(win, (16,16,16,255), self.Box, 0)
         pygame.draw.rect(win, (48,48,48,255), self.Box, 1)
         if self.selected != None:
             pygame.draw.rect(win, (16, 16, 48, 255), self.selected, 0)
+        curLetter = ord('a')        
         for i in enumerate(self.options):
             win.blit(i[1][1], (self.x, self.y + i[0] * self.seperation))
+            curLetter += 1;
     
     def process(self, event):
         if event.type == MOUSEBUTTONDOWN:
@@ -45,3 +53,10 @@ class Context():
                 self.selected = None
             else:
                 self.selected = next(i[2] for i in self.options if i[2].collidepoint(event.pos))
+                
+        if event.type == KEYDOWN:
+            for i in self.options:
+                if event.unicode == i[3]:                        
+                    self.toClose = True
+                    i[0][1]()
+            self.toClose = True
--- src/inventoryDialog.py
+++ src/inventoryDialog.py
@@ -39,30 +39,77 @@ class InventoryDialog(dialog.Dialog):
         titleSize = self.TitleFont.size('Load Out')
         win.blit(self.TitleFont.render('Load Out', True, (255, 255, 255, 255)), (round((win.get_width() - titleSize[0])/2), 15))
         win.blit(self.BodyFont.render('Equipped:', True, (255, 255, 255, 255)), (30, 35))
-        win.blit(self.BodyFont.render('Right Hand:', True, (255, 255, 255, 255)), (30, 55))     
+        win.blit(self.BodyFont.render('Right Hand:', True, (255, 255, 255, 255)), (30, 55))
         win.blit(self.BodyFont.render('Left Hand:', True, (255, 255, 255, 255)), (30, 75))
         
-        win.blit(self.BodyFont.render(self.PC.rightHandEquipped.Description() if self.PC.rightHandEquipped != None else (('(' + self.PC.leftHandEquipped.Description() + ')') if self.PC.leftHandEquipped != None and self.PC.leftHandEquipped.TwoHanded  else 'Empty'), True, (255, 255, 255, 255)), (122, 55))        
-        win.blit(self.BodyFont.render(self.PC.leftHandEquipped.Description() if self.PC.leftHandEquipped != None else (('(' + self.PC.rightHandEquipped.Description() + ')') if self.PC.rightHandEquipped != None and self.PC.rightHandEquipped.TwoHanded  else 'Empty'), True, (255, 255, 255, 255)), (122, 75))
+        win.blit(self.BodyFont.render('(a) ' + self.PC.rightHandEquipped.Description() if self.PC.rightHandEquipped != None else (('(' + self.PC.leftHandEquipped.Description() + ')') if self.PC.leftHandEquipped != None and self.PC.leftHandEquipped.TwoHanded  else 'Empty'), True, (255, 255, 255, 255)), (122, 55))        
+        win.blit(self.BodyFont.render('(b) ' + self.PC.leftHandEquipped.Description() if self.PC.leftHandEquipped != None else (('(' + self.PC.rightHandEquipped.Description() + ')') if self.PC.rightHandEquipped != None and self.PC.rightHandEquipped.TwoHanded  else 'Empty'), True, (255, 255, 255, 255)), (122, 75))
         
         win.blit(self.BodyFont.render('Backpack:', True, (255, 255, 255, 255)), (30, 105))
         
         win.blit(self.BodyFont.render('On Floor:', True, (255, 255, 255, 255)), (30, 255))
         
-        for i in enumerate(self.PC.backpack):           
-            win.blit(self.BodyFont.render(i[1].Description(), True, (255, 255, 255, 255)), (30, 125 + i[0] * 20))
+        # This letter is ascii for b. It'll increment to provide shortcut keys.
+        curLetter = ord('b')        
+        
+        for i in enumerate(self.PC.backpack):
+            curLetter += 1;
+            win.blit(self.BodyFont.render("(" + chr(curLetter) + ") " + i[1].Description(), True, (255, 255, 255, 255)), (30, 125 + i[0] * 20))
             
         for i in enumerate(self.FloorItems):            
-            win.blit(self.BodyFont.render(i[1].Description(), True, (255, 255, 255, 255)), (30, 275 + i[0] * 20))
+            curLetter += 1;
+            win.blit(self.BodyFont.render("(" + chr(curLetter) + ") " + i[1].Description(), True, (255, 255, 255, 255)), (30, 275 + i[0] * 20))
         
         if self.Context != None:
             self.Context.draw(win)
     
+    # There is duplication in this procedure. It might be better served to
+    # seperate some of it into their own procedures.
     def process(self, event):
         if self.Context == None:
             if event.type == KEYDOWN:
+                # Close window
                 if event.key == pygame.K_ESCAPE:
-                    self.toClose = True
+                    self.toClose = True                
+                    
+                # Change right hand equipped
+                elif event.unicode == 'a':
+                    if self.PC.rightHandEquipped != None:
+                        self.Context = context.Context(300,55)
+                        self.Context.AddItem(('Put in backpack', lambda: self.PC.Equip(None, 0)))
+                        self.Context.AddItem(('Drop', lambda: self.PC.Drop(self.PC.rightHandEquipped)))
+                
+                # Change left hand equipped 
+                elif event.unicode == 'b':
+                    if self.PC.leftHandEquipped != None:
+                        self.Context = context.Context(300,75)
+                        self.Context.AddItem(('Put in backpack', lambda: self.PC.Equip(None, 1)))
+                        self.Context.AddItem(('Drop', lambda: self.PC.Drop(self.PC.leftHandEquipped)))    
+
+                # Change inventory or floor
+                elif ord(event.unicode) > ord('b') and ord(event.unicode) < ord('z'):
+                    # Iterate through backpack, then through floor
+                    curLetter = ord('b')
+                    curY = 125
+                    for i in self.PC.backpack:
+                        curLetter += 1
+                        if curLetter == ord(event.unicode):
+                            self.Context = context.Context(300, curY)
+                            self.Context.AddItem(('Equip in Right Hand', lambda: self.PC.Equip(i, 0)))
+                            self.Context.AddItem(('Equip in Left Hand', lambda: self.PC.Equip(i, 1)))
+                            self.Context.AddItem(('Drop', lambda: self.PC.Drop(i)))
+                        curY += 20
+                    
+                    curY = 275
+                    for i in self.FloorItems:
+                        curLetter += 1
+                        if curLetter == ord(event.unicode):
+                            self.Context = context.Context(300, curY)
+                            self.Context.AddItem(('Equip in Right Hand', lambda: self.PC.Equip(i, 0)))
+                            self.Context.AddItem(('Equip in Left Hand', lambda: self.PC.Equip(i, 1)))
+                            if len(self.PC.backpack) < self.PC.backpackSize:
+                                self.Context.AddItem(('Pick up', lambda: self.PC.Pickup(i)))
+                        curY += 20
             
             if event.type == MOUSEBUTTONDOWN:
                 if not self.Box.collidepoint(event.pos):
@@ -102,4 +149,6 @@ class InventoryDialog(dialog.Dialog):
             self.refreshFloorItems()
             if self.Context.toClose == True:
                 self.Context = None
+                
+     
         
