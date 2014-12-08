1387e189c9a9cefd30d454a0644e74e65192cf02
Lachlan Kingsford <lachlan@nerdygentleman.com>
2014-12-08 11:11:40 +1100

    Inventory Bugfix
    Equipping an item in one hand while a two handed item was equipped in
    the other hand did not unequip the other hands item, thus providing a
    cheat of being able to equip a one and a two handed weapon. Fixed.

--- src/character.py
+++ src/character.py
@@ -456,8 +456,14 @@ class Character:
         
     def Equip(self, item, slot):        
         if slot == 0:
+            # Unequip if two handed in other hand
+            if (self.leftHandEquipped != None) and\
+                (self.leftHandEquipped.TwoHanded == True):
+                self.Equip(None, 1)
+            # Unequip
             if (self.rightHandEquipped != None):
                 self.backpack.append(self.rightHandEquipped)
+                
             self.rightHandEquipped = item
             if (item != None and item.TwoHanded):
                 if (self.leftHandEquipped != None):
@@ -465,8 +471,14 @@ class Character:
                     self.leftHandEquipped = None                    
                     
         if slot == 1:
-            if (self.leftHandEquipped != None):
+            # Unequip if two handed in other hand
+            if (self.rightHandEquipped != None) and\
+                    (self.rightHandEquipped.TwoHanded == True):
+                    self.Equip(None, 0)
+            # Unequip
+            if (self.leftHandEquipped != None):                
                 self.backpack.append(self.leftHandEquipped)
+                
             self.leftHandEquipped = item
             if (item != None and item.TwoHanded):
                 if (self.rightHandEquipped != None):
